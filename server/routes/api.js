26c8462eb5387e3885b805e1af160ffbed4cfb9e
didietsuryadi <didietsuryadi>
2017-03-17 09:01:01 +0700

    create-end-point

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,21 @@
 var express = require('express')
 var router = express.Router()
-
+var userControllers = require('../controllers/UserController.js')
+var challengeControllers = require('../controllers/ChallengeController.js')
 /* GET users listing. */
-router.get('/', function (req, res, next) {})
+//routing user
+router.get('/user', userControllers.getUsers)
+router.get('/user/:id', userControllers.getUser)
+router.post('/user', userControllers.createUser)
+router.put('/user/:id', userControllers.updateUser)
+router.delete('/user/:id', userControllers.deleteUser)
+
+//routing challenge
+router.get('/challenge', challengeControllers.getChallenges)
+router.get('/challenge/:id', challengeControllers.getChallenge)
+router.post('/challenge', challengeControllers.createChallenge)
+router.put('/challenge/:id', challengeControllers.updateChallenge)
+router.delete('/challenge/:id', challengeControllers.deleteChallenge)
+
 
 module.exports = router
