6bc8b7cee0c3360b230b5a048209150984cd0202
Eri Irawan <eryirawan91@gmail.com>
2017-03-17 09:20:08 +0700

    Revert "add controller"

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('using /api')
+  res.send({message: 'It is index'})
 })
 
 module.exports = router
